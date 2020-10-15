44c2602a27e41b1bfd09e604b48c0745bef14b23
Klaudiusz Skowroński <claude-plos@o2.pl>
2020-10-15 17:41:44 +0200

    v01

---
+++ src/main/java/pl/kskowronski/Application.java
@@ -0,0 +1,26 @@
+package pl.kskowronski;
+
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.server.PWA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.vaadin.artur.helpers.LaunchUtil;
+
+/**
+ * The entry point of the Spring Boot application.
+ *
+ * Use the  * and some desktop browsers.
+ *
+ */
+@SpringBootApplication(exclude = ErrorMvcAutoConfiguration.class)
+@PWA(name = "employee_portal", shortName = "employee_portal")
+public class Application extends SpringBootServletInitializer implements AppShellConfigurator {
+
+    public static void main(String[] args) {
+        LaunchUtil.launchBrowserInDevelopmentMode(SpringApplication.run(Application.class, args));
+    }
+
+}
